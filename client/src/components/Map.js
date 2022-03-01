b2c84a94281dcd80c5b5bca1dd70854d8743ec7a
NateLoken <nfloken@comcast.net>
2022-03-01 06:09:59 +0000

    refactored map component to use official hook design

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,26 +1,42 @@
 import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
-const Map = () => {
-  const mapStyles = {
-    height: "100vh",
-    width: "100%",
-  };
-
-  const defaultCenter = {
-    lat: 32.316238,
-    lng: -106.780449,
-  };
-
-  return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-      <GoogleMap
-        mapContainerStyle={mapStyles}
-        zoom={13}
-        center={defaultCenter}
-      />
-    </LoadScript>
-  );
+const options = {
+  disableDefaultUI: true,
+  zoom: 13
 };
 
-export default Map;
+const mapStyles = {
+  height: "100vh",
+  width: "100%"
+};
+
+const defaultCenter = {
+  lat: 32.316238,
+  lng: -106.780449
+};
+
+function Map() {
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: "google-maps-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  });
+
+  const renderMap = () => {
+    return (
+      <GoogleMap
+        options={options}
+        mapContainerStyle={mapStyles}
+        center={defaultCenter}
+      />
+    );
+  };
+
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry</div>;
+  }
+
+  return isLoaded ? renderMap() : <h2>Loading...</h2>;
+}
+
+export default React.memo(Map);
