e232ce12afcbda1f673d69a5335e09cfa79169b1
saintjeus <saintjeus@gmail.com>
2022-03-02 20:29:46 -0700

    map centers on user location

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Session from "./components/Session";
 import Map from "./components/Map";
 import Navbar from "./components/Navbar";
@@ -6,12 +6,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
+  const [coordinates, setCoordinates] = useState({});
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}})=>{
+      setCoordinates({lat:latitude, lng:longitude});
+    });
+  }, []);
   return (
     <Router>
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Map/>} />
+          <Route path="/" exact element={<Map
+          setCoordinates={setCoordinates}
+          coordinates={coordinates}
+          />} />
           <Route path="/session" element={<Session/>} />
         </Routes>
       </div>
