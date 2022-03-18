f5cd303310860994f8c614bb32072b2d18702b6a
NateLoken <nfloken@comcast.net>
2022-03-18 14:57:47 -0600

    added info window for on click of the markers

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  InfoWindow,
+  Marker,
+  useJsApiLoader,
+} from "@react-google-maps/api";
 
 const options = {
   disableDefaultUI: true,
@@ -13,60 +18,76 @@ const mapStyles = {
 
 const libraries = ["places"];
 
-const bars = [];
-
 function Map() {
   const { isLoaded, loadError } = useJsApiLoader({
     id: "google-maps-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
+  const [bars, setBars] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
 
   const mapRef = React.useRef();
-  const onLoad = React.useCallback(
-    (map) => {
-      mapRef.current = map;
+  const onLoad = React.useCallback((map) => {
+    mapRef.current = map;
 
-      navigator.geolocation.getCurrentPosition((position) => {
-        const localArea = new window.google.maps.LatLng(
-          position.coords.latitude,
-          position.coords.longitude
-        );
+    navigator.geolocation.getCurrentPosition((position) => {
+      const localArea = new window.google.maps.LatLng(
+        position.coords.latitude,
+        position.coords.longitude
+      );
 
-        mapRef.current.setCenter(localArea);
+      mapRef.current.setCenter(localArea);
 
-        const request = {
-          location: localArea,
-          radius: "5000",
-          type: ["bar"],
-        };
+      const request = {
+        location: localArea,
+        radius: "5000",
+        type: ["bar"],
+      };
 
-        const service = new window.google.maps.places.PlacesService(
-          mapRef.current
-        );
+      const service = new window.google.maps.places.PlacesService(
+        mapRef.current
+      );
 
-        service.nearbySearch(request, (results, status) => {
-          if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-              const marker = new window.google.maps.Marker({
-                position: results[i].geometry.location,
-                map: mapRef.current,
-                key: results[i].place_id,
-              });
-            }
-          }
-        });
+      service.nearbySearch(request, (results, status) => {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+          setBars(results);
+        }
       });
-    },
-    []
-  );
+    });
+  }, []);
 
   if (loadError) return "Error Loading Maps";
   if (!isLoaded) return "Loading Maps";
 
   return (
     <GoogleMap options={options} mapContainerStyle={mapStyles} onLoad={onLoad}>
-      {}
+      {bars.map((item) => {
+        return (
+          <Marker
+            key={item.place_id}
+            position={item.geometry.location}
+            onClick={() => setSelected(item)}
+          />
+        );
+      })}
+
+      {console.log(selected)}
+
+      {selected ? (
+        <InfoWindow
+          position={selected.geometry.location}
+          onCloseClick={() => {
+            setSelected(null);
+          }}
+        >
+          <div>
+            <h2>{selected.name}</h2>
+            <h2>{selected.rating}</h2>
+            <h2>{selected.price_level}</h2>
+          </div>
+        </InfoWindow>
+      ) : null}
     </GoogleMap>
   );
 }
