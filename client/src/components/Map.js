8e54b4831831ed7e338db2e4caa0ec80e473aba6
NateLoken <nfloken@comcast.net>
2022-03-18 13:23:21 -0600

    Added google places call to the map component to render in markers to the map based on current location

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,42 +1,74 @@
 import React from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 const options = {
   disableDefaultUI: true,
-  zoom: 13
+  zoom: 13,
 };
 
 const mapStyles = {
   height: "100vh",
-  width: "100%"
+  width: "100%",
 };
 
-const defaultCenter = {
-  lat: 32.316238,
-  lng: -106.780449
-};
+const libraries = ["places"];
 
-function Map({setCoordinates, coordinates}) {
+const bars = [];
+
+function Map() {
   const { isLoaded, loadError } = useJsApiLoader({
     id: "google-maps-script",
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
   });
 
-  const renderMap = () => {
-    return (
-      <GoogleMap
-        options={options}
-        mapContainerStyle={mapStyles}
-        center={coordinates}
-      />
-    );
-  };
+  const mapRef = React.useRef();
+  const onLoad = React.useCallback(
+    (map) => {
+      mapRef.current = map;
 
-  if (loadError) {
-    return <div>Map cannot be loaded right now, sorry</div>;
-  }
+      navigator.geolocation.getCurrentPosition((position) => {
+        const localArea = new window.google.maps.LatLng(
+          position.coords.latitude,
+          position.coords.longitude
+        );
 
-  return isLoaded ? renderMap() : <h2>Loading...</h2>;
+        mapRef.current.setCenter(localArea);
+
+        const request = {
+          location: localArea,
+          radius: "5000",
+          type: ["bar"],
+        };
+
+        const service = new window.google.maps.places.PlacesService(
+          mapRef.current
+        );
+
+        service.nearbySearch(request, (results, status) => {
+          if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              const marker = new window.google.maps.Marker({
+                position: results[i].geometry.location,
+                map: mapRef.current,
+                key: results[i].place_id,
+              });
+            }
+          }
+        });
+      });
+    },
+    []
+  );
+
+  if (loadError) return "Error Loading Maps";
+  if (!isLoaded) return "Loading Maps";
+
+  return (
+    <GoogleMap options={options} mapContainerStyle={mapStyles} onLoad={onLoad}>
+      {}
+    </GoogleMap>
+  );
 }
 
 export default React.memo(Map);
