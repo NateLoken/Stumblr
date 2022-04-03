d66e69ee1f0b8ed58247f343a4d9df7d603d982e
Avery Lee <averyl36@yahoo.com>
2022-04-03 10:44:05 -0600

    Added tab to alternate between list and map view

--- client/src/components/FindBars.js
+++ client/src/components/FindBars.js
@@ -6,6 +6,52 @@ import {
   Marker,
   useJsApiLoader,
 } from "@react-google-maps/api";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+
+
+
+
+
+
+
 
 const options = {
   disableDefaultUI: true,
@@ -24,7 +70,7 @@ const mapBeGone = {
 
 const libraries = ["places"];
 
-const isMapView = false;
+const isMapView = true;
 
 function Map() {
   const { isLoaded, loadError } = useJsApiLoader({
@@ -34,6 +80,12 @@ function Map() {
   });
   const [selected, setSelected] = React.useState(null);
   const [bars, setBars] = React.useState([]);
+
+  // To control the tabs
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   const mapRef = React.useRef();
   const onLoad = React.useCallback((map) => {
@@ -68,60 +120,60 @@ function Map() {
   if (loadError) return "Error Loading Maps";
   if (!isLoaded) return "Loading Maps";
 
-  // Displays map view of bar if it is selected
-  if (isMapView) {
-    return (
-      <div>
-        <GoogleMap
-          options={options}
-          mapContainerStyle={mapStyles}
-          onLoad={onLoad}
-        >
-          {bars.map((item) => {
-            return (
-              <Marker
-                key={item.place_id}
-                position={item.geometry.location}
-                onClick={() => setSelected(item)}
-              />
-            );
-          })}
+  return (
+    <div>
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs variant="fullWidth" value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="Map" {...a11yProps(0)} />
+            <Tab label="List" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <GoogleMap
+            options={options}
+            mapContainerStyle={mapStyles}
+            onLoad={onLoad}
+          >
+            {bars.map((item) => {
+              return (
+                <Marker
+                  key={item.place_id}
+                  position={item.geometry.location}
+                  onClick={() => setSelected(item)}
+                />
+              );
+            })}
 
-          {selected ? (
-            <InfoWindow
-              position={selected.geometry.location}
-              onCloseClick={() => {
-                setSelected(null);
-              }}
-            >
-              <div>
-                <h2>{selected.name}</h2>
-                <h2>{selected.rating}</h2>
-                <h2>{selected.price_level}</h2>
-              </div>
-            </InfoWindow>
-          ) : null}
-        </GoogleMap>
-        <br />
-      </div>
-    );
-  }
-
-  // Displays list view of bar if it is selected
-  if (!isMapView) {
-    return (
-      <div>
-        {/* Must render fake map in order to populate bar list */}
-        <GoogleMap
-          options={options}
-          mapContainerStyle={mapBeGone}
-          onLoad={onLoad}
-        >
-        </GoogleMap>
-        <ListBars bars={bars} />
-      </div>
-    );
-  }
+            {selected ? (
+              <InfoWindow
+                position={selected.geometry.location}
+                onCloseClick={() => {
+                  setSelected(null);
+                }}
+              >
+                <div>
+                  <h2>{selected.name}</h2>
+                  <h2>{selected.rating}</h2>
+                  <h2>{selected.price_level}</h2>
+                </div>
+              </InfoWindow>
+            ) : null}
+          </GoogleMap>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <GoogleMap
+            options={options}
+            mapContainerStyle={mapBeGone}
+            onLoad={onLoad}
+          >
+          </GoogleMap>
+          <ListBars bars={bars} />
+        </TabPanel>
+      </Box>
+      <br />
+    </div>
+  );
 }
 
 export default React.memo(Map);
