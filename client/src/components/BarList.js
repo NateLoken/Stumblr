2d840443953b4abcac1223dbfbaa5c2ab7e8cce4
Avery Lee <averyl36@yahoo.com>
2022-04-05 20:34:54 -0600

    Refactored the list view display

--- client/src/components/BarList.js
+++ client/src/components/BarList.js
@@ -1,14 +1,77 @@
-// Displays bars in list format when user is doing a search
-import React from "react";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Rating from '@mui/material/Rating';
+
+const Spacer = require('react-spacer')
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 function ListBars({ bars }) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const [value, setValue] = React.useState(2);
+
+  console.log(bars)
+
   return (
     <ul>
       {bars && bars.length > 0 ? (
         bars.map((bars) => {
           return <li key={bars.place_id}>
-				<h4>{bars.name}, rating: {bars.rating}, price level: {bars.price_leve}</h4>
-		 </li>;
+            <Card>
+              <CardHeader title={bars.name} />
+              <Divider />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    Rating: {bars.rating} ({bars.user_ratings_total})
+                    <br />
+                    <Rating text-align='right' name="read-only" size="small" precision={0.5} value={bars.rating} readOnly />
+                    <br />Price level: {bars.price_level} 
+                    <br />
+                    
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+                <ExpandMore
+                  expand={expanded}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                >
+                  <ExpandMoreIcon />
+                </ExpandMore>
+              </CardActions>
+              <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <Typography variant="body2" color="text.secondary">
+                    Additional Info can be put here
+                  </Typography>
+                </CardContent>
+              </Collapse>
+            </Card>
+          </li>;
         })
       ) : (
         <li>No bars in area</li>
