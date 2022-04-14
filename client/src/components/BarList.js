6ded6312d02101f8fe23b5036260f444f58d968d
Devin Thaker <devinthaker@gmail.com>
2022-04-14 01:57:54 -0600

    m0re refactoring of list view

--- client/src/components/BarList.js
+++ client/src/components/BarList.js
@@ -3,7 +3,6 @@ import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import Rating from '@mui/material/Rating';
